c5c030190ad49a8415e2ff4a10a31b654ec4c7ae
Nithyanandam <alaghusundhar@hotmail.com>
2022-01-07 14:58:43 -0600

    Fix checkout variable

--- vars/checkoutGitRepo.groovy
+++ vars/checkoutGitRepo.groovy
@@ -4,7 +4,7 @@ void call(Map config = [:]){
     String branchName = config.branchName ?: 'master'
     dir(config.buildDirectoryName) {
         String gitUrl = Constants.GITHUB_TOOLS_URL + '/' + config.repositoryName
-        git credentialId: config.credentialName, url: gitUrl, branch: buildDirectoryName
+        git credentialId: config.credentialName, url: gitUrl, branch: config.buildDirectoryName
         sh script: 'git config user.email :jenkinsCICD@sap.com'
         sh script: "git config user.name ${config.credentialName}"
     }
