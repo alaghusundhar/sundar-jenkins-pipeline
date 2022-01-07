efe57f282dbcb918f8b352817313cebcf5aace76
Nithyanandam <alaghusundhar@hotmail.com>
2022-01-07 09:55:16 -0600

    Fixing the issues with pipeline

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -6,15 +6,23 @@ void call(body){
         pipelines: ''
     ]
     pipeline {
-        parallel {
-            stage('Checkout Production Configuration Repository') {
-                script {
-                    checkoutGitRepo(repositoryName: 'hello-kubernetes-prod-config', credentialName: GITHUB_TOOLS_PROD_CONFIG_TOKEN_CREDENTIAL_NAME, buildDirectoryName: 'hello-kubernetes-prod-config')
+        stage ('Checkout Code') {
+            parallel {
+                stage('Checkout Production Configuration Repository') {
+                    steps {
+                        script {
+                            checkoutGitRepo(repositoryName: 'hello-kubernetes-prod-config', credentialName: GITHUB_TOOLS_CREDENTIAL_NAME, buildDirectoryName: 'hello-kubernetes-prod-config')
+                        }
+                    }
                 }
-            }
-            stage ('Checkout Development Configuration Repository') {
-                    checkoutGitRepo(repositoryName: 'hello-kubernetes-dev-config', credentialName: GITHUB_TOOLS_CREDENTIAL_NAME, buildDirectoryName: 'hello-kubernetes-dev-config')
-            }
+                stage ('Checkout Development Configuration Repository') {
+                    steps {
+                        script {
+                            checkoutGitRepo(repositoryName: 'hello-kubernetes-dev-config', credentialName: GITHUB_TOOLS_CREDENTIAL_NAME, buildDirectoryName: 'hello-kubernetes-dev-config')
+                        }
+                    }
+                }
+            }    
         }
     }
 }
