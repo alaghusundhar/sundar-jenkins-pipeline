92df36d9c336b088d571de6655bf8c9091a2bc6d
Nithyanandam <alaghusundhar@hotmail.com>
2022-01-07 10:29:31 -0600

    Fixing issues

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,16 +1,14 @@
 import static com.kubernetes.Constants.GITHUB_TOOLS_CREDENTIAL_NAME
 import static com.kubernetes.Constants.GITHUB_TOOLS_PROD_CONFIG_TOKEN_CREDENTIAL_NAME
 
-void call(body){
-    Map config = [
-        pipelines: ''
-    ]
-    pipeline {
-        agent any
-        stages {
-            stage ("Checkout Code") {
+pipeline { 
+    agent any
+    stages {
+        stage ("Checkout Code"){
+            steps {
                 script {
                     checkoutGitRepo(repositoryName: 'hello-kubernetes-prod-config', credentialName: GITHUB_TOOLS_CREDENTIAL_NAME, buildDirectoryName: 'hello-kubernetes-prod-config')
+                    }
                 }
             }
         }
