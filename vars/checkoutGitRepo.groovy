013f4cf45e7dfbeb0e6150f5865caae8eb5a9817
alaghusundhar <46031830+alaghusundhar@users.noreply.github.com>
2022-01-07 15:17:17 -0600

    Update checkoutGitRepo.groovy

--- vars/checkoutGitRepo.groovy
+++ vars/checkoutGitRepo.groovy
@@ -4,7 +4,7 @@ void call(Map config = [:]){
     String branchName = config.branchName ?: 'main'
     dir(config.buildDirectoryName) {
         String gitUrl = Constants.GITHUB_TOOLS_URL + '/' + config.repositoryName
-        git credentialId: config.credentialName, url: gitUrl, branch: branchName, config.buildDirectoryName
+        git credentialId: config.credentialName, url: gitUrl, branch: branchName
         sh script: 'git config user.email :jenkinsCICD@sap.com'
         sh script: "git config user.name ${config.credentialName}"
     }
